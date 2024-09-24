8abe2a611de1ad6265e4648c379b362ae676bdcb
ansep <andreasepielli97@gmail.com>
2024-09-24 13:15:40 +0200

    Connected API for groupbuy list

--- frontend/src/app/open-groups-list/open-groups-list.component.ts
+++ frontend/src/app/open-groups-list/open-groups-list.component.ts
@@ -10,15 +10,52 @@ import { Router } from '@angular/router';
   styleUrl: './open-groups-list.component.scss',
 })
 export class OpenGroupsListComponent {
-  items: any[] = [];
+  items: {
+    id: number;
+    title: string;
+    unitPrice: number;
+    product: string;
+    availablePieces: number;
+    category: string;
+    location: string;
+    image: string;
+    description: string;
+    subscribedPeople: number;
+    requiredPeople: number;
+  }[] = [];
+
   constructor(private apiservice: ApiService, private router: Router) {}
   ngOnInit() {
-    this.items = this.apiservice.getOpenGroups();
+    // this.items = this.apiservice.getOpenGroups();
+    this.apiservice.getOpenGroups().subscribe((data: any) => {
+      if (
+        data['_embedded'] &&
+        data['_embedded'].groupBuys &&
+        data['_embedded'].groupBuys.length > 0
+      ) {
+        this.items = data['_embedded'].groupBuys.map((groupBuy: any) => {
+          return {
+            id: groupBuy._links.self[0].href.split('/').pop(),
+            title: groupBuy.description,
+            unitPrice: groupBuy.cost,
+            product: groupBuy.product,
+            availablePieces: groupBuy.maxSize,
+            category: groupBuy.category,
+            location: groupBuy.location,
+            image: groupBuy.postingPicturePath,
+            //TODO: Add real description, subscribedPeople, requiredPeople when implemented in API
+            description:
+              'This is a description of the product that is being sold in the group buy listing. It is a very long and verbose description that contains nothing relevant at all. Yeah indeed this piece of text is very long and pretty useless but it is a great way to understand how bad frontend can go I guess.',
+            subscribedPeople: 50,
+            requiredPeople: 1000,
+          };
+        });
+      }
+    });
   }
 
   // Function to load the product details page
-  loadProduct(arg0: any) {
-    this.router.navigate(['/buyer/group', arg0]);
-    console.log('Product Clicked : ', arg0);
+  openGroupBuy(id: number) {
+    this.router.navigate([localStorage.getItem('role'), 'group', id]);
   }
 }
--- frontend/src/app/services/api.service.ts
+++ frontend/src/app/services/api.service.ts
@@ -8,27 +8,28 @@ import { HttpClient } from '@angular/common/http';
 export class ApiService {
   constructor(private https: HttpClient) {}
   getOpenGroups() {
-    const openGroups = [];
+    return this.https.get('http://localhost:8080/api/auth/groupbuy');
+    // const openGroups = [];
 
-    for (let i = 0; i < 10; i++) {
-      const groupBuy = {
-        id: i,
-        title: `Group Buy nr  ${i + 1}`,
-        description: `Description for Group Buy ${
-          i + 1
-        }. This is a very long and verbose decription that contains nothing relevant at all. Yeah indeed this piece of text is very long and pretty useless but it's a great way to understand how bad frontend can go I guess.`,
-        subscribedPeople: Math.floor(Math.random() * 500),
-        requiredPeople: Math.floor(Math.random() * 5000) + 100,
-        unitPrice: parseFloat((Math.random() * 100).toFixed(1)),
-        brokerName: `Broker ${i + 1}`,
-        image:
-          'https://media-assets.wired.it/photos/64f5bd6bcceb534d9f169474/16:9/w_1888,h_1062,c_limit/notebook%20da%20gaming.jpg',
-        location: `Location ${i + 1}`,
-      };
-      openGroups.push(groupBuy);
-    }
+    // for (let i = 0; i < 10; i++) {
+    //   const groupBuy = {
+    //     id: i,
+    //     title: `Group Buy nr  ${i + 1}`,
+    //     description: `Description for Group Buy ${
+    //       i + 1
+    //     }. This is a very long and verbose decription that contains nothing relevant at all. Yeah indeed this piece of text is very long and pretty useless but it's a great way to understand how bad frontend can go I guess.`,
+    //     subscribedPeople: Math.floor(Math.random() * 500),
+    //     requiredPeople: Math.floor(Math.random() * 5000) + 100,
+    //     unitPrice: parseFloat((Math.random() * 100).toFixed(1)),
+    //     brokerName: `Broker ${i + 1}`,
+    //     image:
+    //       'https://media-assets.wired.it/photos/64f5bd6bcceb534d9f169474/16:9/w_1888,h_1062,c_limit/notebook%20da%20gaming.jpg',
+    //     location: `Location ${i + 1}`,
+    //   };
+    //   openGroups.push(groupBuy);
+    // }
 
-    return openGroups;
+    // return openGroups;
   }
 
   getParticipants(): any {
