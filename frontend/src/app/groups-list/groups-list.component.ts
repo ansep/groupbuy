1b592c45c165c3282b85729542a2657494943388
ansep <andreasepielli97@gmail.com>
2024-09-25 14:29:02 +0200

    implemented list search filtering logic

--- frontend/src/app/groups-list/groups-list.component.ts
+++ frontend/src/app/groups-list/groups-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, SimpleChanges } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-groups-list',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './groups-list.component.html',
   styleUrl: './groups-list.component.scss',
 })
@@ -22,12 +23,52 @@ export class GroupsListComponent {
     subscribedPeople: number;
     requiredPeople: number;
   }[] = [];
+  displayedGroups: {
+    id: number;
+    title: string;
+    unitPrice: number;
+    product: string;
+    availablePieces: number;
+    category: string;
+    location: string;
+    image: string;
+    description: string;
+    subscribedPeople: number;
+    requiredPeople: number;
+  }[] = [];
+  searchForm = new FormGroup({
+    search: new FormControl(null),
+  });
   placeHolderImage = 'assets/no-image-available.png';
 
   constructor(private router: Router) {}
 
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['groups']) {
+      this.displayedGroups = this.groups;
+    }
+  }
+  ngAfterViewInit() {
+    this.searchForm.get('search')?.valueChanges.subscribe((searchTerm) => {
+      this.filterGroups(searchTerm);
+    });
+  }
+
   // Function to load the product details page
   openGroupBuy(id: number) {
     this.router.navigate([localStorage.getItem('role'), 'group', id]);
+  }
+
+  filterGroups(term: string | null) {
+    if (!term) {
+      this.displayedGroups = this.groups;
+      return;
+    }
+    this.displayedGroups = this.groups.filter((group: any) => {
+      return (
+        group.title.toLowerCase().includes(term.toLowerCase()) ||
+        group.description.toLowerCase().includes(term.toLowerCase())
+      );
+    });
   }
 }
